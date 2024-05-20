80cc19a1c27a6dc52e40c972a05f6b5e00344a3d
Nelson Feijó <114948173+nelsonfeijo@users.noreply.github.com>
2024-05-20 09:58:36 -0300

    estrutura do menu

--- src/components/page/Menu/Menu.jsx
+++ src/components/page/Menu/Menu.jsx
@@ -1,13 +1,41 @@
 import styles from "./Menu.module.css";
+import doc1 from '../../../assets/barca_de_docinhos.jpg'
+import bolo1 from '../../../assets/bolo_de_pote.jpg'
+import bolo2 from '../../../assets/bolo_de_leite_ninho.jpg'
+import bolo3 from '../../../assets/brownie.jpg'
+import bolo4 from '../../../assets/bolodepote.jpg'
+import doc2 from '../../../assets/minibrownie.jpg'
+import doc3 from '../../../assets/docinhodecoco.jpg'
+import doc4 from '../../../assets/caixinhadedocinhos.jpg'
+import { BiCartAdd } from "react-icons/bi";
 
 function Menu() {
     return ( 
         <div className={styles.menu_container}>
         <section className={styles.menu_section} id="starters">
-            <h2>Bolos</h2>
+            <h1>Bolos</h1>
             <div className={styles.menu_item}>
                 <div>
-                    <img></img>
+                <img src={bolo1}></img>
+                </div>
+                <div>
+                    
+                    <div>
+                    <h3>Com cobertura de chocolate</h3>
+                    <p>Alface romana, croutons e molho Caesar caseiro.</p>
+                    <div className={styles.price}>
+                        <p>R$ 15,00</p>
+                    <button><BiCartAdd /></button>
+                    </div>
+                    </div>
+                </div>
+                
+            </div>
+            <div className={styles.menu_item}>
+            <div>
+                <img src={bolo2}></img>
+                </div>
+                <div>
                     <div>
                     <h3>Com cobertura de chocolate</h3>
                     <p>Alface romana, croutons e molho Caesar caseiro.</p>
@@ -17,11 +45,29 @@ function Menu() {
                     </div>
                     </div>
                 </div>
-                
+            </div>
+            <h2>Bolos de Pote</h2>
+            <div className={styles.menu_item}>
+            <div>
+                <img src={bolo3}></img>
+                </div>
+            <div>
+                    
+                    <div>
+                    <h3>Com cobertura de chocolate</h3>
+                    <p>Alface romana, croutons e molho Caesar caseiro.</p>
+                    <div className={styles.price}>
+                        <p>R$ 15,00</p>
+                    <button><i></i></button>
+                    </div>
+                    </div>
+                </div>
             </div>
             <div className={styles.menu_item}>
+            <div>
+                <img src={bolo4}></img>
+                </div>
                 <div>
-                    <img></img>
                     <div>
                     <h3>Com cobertura de chocolate</h3>
                     <p>Alface romana, croutons e molho Caesar caseiro.</p>
@@ -35,10 +81,45 @@ function Menu() {
         </section>
 
         <section className={styles.menu_section} id="main-courses">
+        <h1>Bolos de Pote</h1>
+            <div className={styles.menu_item}>
+            <div>
+                <img src={doc1}></img>
+                </div>
+                 <div>   
+                    <div>
+                    <h3>Com cobertura de chocolate</h3>
+                    <p>Alface romana, croutons e molho Caesar caseiro.</p>
+                    <div className={styles.price}>
+                        <p>R$ 15,00</p>
+                    <button><i></i></button>
+                    </div>
+                    </div>
+                </div>
+            </div>
+            <div className={styles.menu_item}>
+            <div>
+                <img src={doc2}></img>
+                </div>
+            <div>
+                    
+                    <div>
+                    <h3>Com cobertura de chocolate</h3>
+                    <p>Alface romana, croutons e molho Caesar caseiro.</p>
+                    <div className={styles.price}>
+                        <p>R$ 15,00</p>
+                    <button><i></i></button>
+                    </div>
+                    </div>
+                </div>
+            </div>
             <h2>Bolos de Pote</h2>
             <div className={styles.menu_item}>
             <div>
-                    <img></img>
+                <img src={doc3}></img>
+                </div>
+            <div>
+                
                     <div>
                     <h3>Com cobertura de chocolate</h3>
                     <p>Alface romana, croutons e molho Caesar caseiro.</p>
@@ -51,7 +132,9 @@ function Menu() {
             </div>
             <div className={styles.menu_item}>
             <div>
-                    <img></img>
+                <img src={doc4}></img>
+                </div>
+            <div>
                     <div>
                     <h3>Com cobertura de chocolate</h3>
                     <p>Alface romana, croutons e molho Caesar caseiro.</p>
@@ -62,6 +145,7 @@ function Menu() {
                     </div>
                 </div>
             </div>
+            
         </section>
     </div>
     );
